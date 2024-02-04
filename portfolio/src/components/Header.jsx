a46d2978141f32e3521d2441d6c1ae8d860f726d
fergarperez <fgarfella1@gmail.com>
2024-02-05 00:48:02 +0100

    add: Header component & Tailwind.css

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,18 @@
+export function Header() {
+    return (
+    <header class="navbar">
+        <img src="../assets/images/animated-photo.png"
+        alt="Photo of myself in an animated way"
+        class="photoCover" /> 
+    
+        <nav class="navLinks">
+            <ul>
+                <li><a href="#main-about-me" class="links-nav">About me</a></li>
+                <li><a href="#main-projects" class="links-nav">Projects</a></li>
+                <li><a href="#main-skills" class="links-nav">Skills</a></li>
+                <li><a href="#main-contact-me" class="links-nav">Contact me</a></li>
+            </ul>
+        </nav>
+    </header>
+    )
+}
